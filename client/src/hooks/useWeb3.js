350dfa207720d26f4a32993bc52e77a240154ce5
kryptowiz <44969751+kryptowiz@users.noreply.github.com>
2021-03-04 15:53:10 +0100

    fix web3 hook

--- client/src/hooks/useWeb3.js
+++ client/src/hooks/useWeb3.js
@@ -11,47 +11,53 @@ export const Web3Provider = ({ children }) => {
   const [accounts, setAccounts] = useState([]);
   const [contract, setContract] = useState([]);
 
-  useEffect(() => {
-    const init = async () => {
-      try {
+  const init = async () => {
+    try {
       // Get network provider and web3 instance.
-        const web3Instance = await getWeb3();
+      const web3Instance = await getWeb3();
 
-        // Use web3 to get the user's accounts.
-        const accountsInstance = await web3Instance.eth.getAccounts();
+      // Use web3 to get the user's accounts.
+      const accountsInstance = await web3Instance.eth.getAccounts();
 
-        // Get the contract instance.
-        const networkId = await web3Instance.eth.net.getId();
-        const deployedNetwork = SimpleStorageContract.networks[networkId];
-        const instance = new web3Instance.eth.Contract(
-          SimpleStorageContract.abi,
-          deployedNetwork && deployedNetwork.address,
-        );
+      // Get the contract instance.
+      const networkId = await web3Instance.eth.net.getId();
+      const deployedNetwork = SimpleStorageContract.networks[networkId];
+      const instance = new web3Instance.eth.Contract(
+        SimpleStorageContract.abi,
+        deployedNetwork && deployedNetwork.address,
+      );
 
-        setWeb3(web3Instance);
-        setAccounts(accountsInstance);
-        setContract(instance);
-      } catch (error) {
-        alert('Failed to load web3');
-        console.log(error);
-      }
-    };
+      setWeb3(web3Instance);
+      setAccounts(accountsInstance);
+      setContract(instance);
+    } catch (error) {
+      // Figure out what to do here...
+    }
+  };
 
+  useEffect(() => {
     init();
   }, []);
 
   return (
-    <Web3Context.Provider value={{ web3, contract, accounts }}>
+    <Web3Context.Provider value={{
+      web3, contract, accounts, init,
+    }}
+    >
       {children}
     </Web3Context.Provider>
   );
 };
 
 export const useWeb3 = () => {
-  const { web3, accounts, contract } = useContext(Web3Context);
+  const {
+    web3, accounts, contract, init,
+  } = useContext(Web3Context);
+
   return {
     web3,
     accounts,
     contract,
+    init,
   };
 };
